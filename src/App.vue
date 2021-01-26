f21e1b4bf27be4fd2cd6b33a7573581702d9e49f
chrisobo98 <christopherbermudez98@gmail.com>
2021-01-25 19:09:13 -0500

    set up navbar with icons

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <v-app>
-    <Navbar/>
+    <Navbar />
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     Navbar,
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <v-navigation-drawer
+      v-model="drawer"
+      dark
+      app
+      mini-variant
+      class="pink accent-2"
+    >
+      <v-list tile>
+        <v-list-item class="mb-8" router to="/">
+          <v-list-item-action>
+            <v-icon>fas fa-truck-loading</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+        <v-list-item class="mb-8" router to="/">
+          <v-list-item-action>
+            <v-icon>fas fa-address-card</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+        <v-list-item class="mb-8" router to="/">
+          <v-list-item-action>
+            <v-icon>fas fa-tasks</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+        <v-list-item class="mb-8" router to="/">
+          <v-list-item-action>
+            <v-icon>fas fa-tools</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
